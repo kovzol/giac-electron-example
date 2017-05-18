6f3090f8ac7ca658cefbb816ffe5f6e624426a98
kovzol <zoltan@geogebra.org>
2017-05-18 11:19:15 +0200

    Attempt to be platform independent

---
+++ install.js
@@ -0,0 +1,26 @@
+var fs_node = require('fs');
+var fs = require('file-system');
+var child_process = require('child_process');
+
+function spawn(cmd, args, opt) {
+    var isWindows = /win/.test(process.platform);
+
+    if ( isWindows ) {
+        if ( !args ) args = [];
+        args.unshift(cmd);
+        args.unshift('/c');
+        cmd = process.env.comspec;
+    }
+
+    return child_process.spawn(cmd, args, opt);
+}
+
+var cmd = spawn("node_modules/.bin/electron-rebuild");
+fs.mkdir('build', [0755], function(err) {});
+var giac_node_bin_folder = 'node_modules/giac/bin/';
+fs_node.readdirSync(giac_node_bin_folder).forEach(file => {
+  console.log("Copying " + giac_node_bin_folder + file + "/giac.node to the build folder");
+  fs.copyFile(giac_node_bin_folder + file + '/giac.node', 'build/giac.node', function(err) {
+    console.err("Cannot copy file");
+    });
+})
