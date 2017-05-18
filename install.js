48bbcfa06699545921a7e3954d8d68ef15318b97
GeoGebra Developer <geogebra@GeoGebras-Mac-mini.local>
2017-05-18 18:41:51 +0200

    Fix Mac build, fix spawn to be synchronous

--- install.js
+++ install.js
@@ -2,8 +2,8 @@ var fs_node = require('fs');
 var fs = require('file-system');
 var child_process = require('child_process');
 
-function spawn(cmd, args, opt) {
-    var isWindows = /win/.test(process.platform);
+function spawnSync(cmd, args, opt) {
+    var isWindows = /^win/.test(process.platform);
 
     if ( isWindows ) {
         if ( !args ) args = [];
@@ -12,10 +12,10 @@ function spawn(cmd, args, opt) {
         cmd = process.env.comspec;
     }
 
-    return child_process.spawn(cmd, args, opt);
+    return child_process.spawnSync(cmd, args, opt);
 }
 
-var cmd = spawn("node_modules/.bin/electron-rebuild");
+var cmd = spawnSync("node_modules/.bin/electron-rebuild");
 fs.mkdir('build', [0755], function(err) {});
 var giac_node_bin_folder = 'node_modules/giac/bin/';
 fs_node.readdirSync(giac_node_bin_folder).forEach(file => {
